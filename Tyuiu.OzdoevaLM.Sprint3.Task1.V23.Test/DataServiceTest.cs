881945e1a39fcdb20e83117552b568863f5fd1da
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-11-07 22:22:30 +0500

    Task1

---
+++ Tyuiu.OzdoevaLM.Sprint3.Task1.V23.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.OzdoevaLM.Sprint3.Task1.V23.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint3.Task1.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 5;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+
+            double wait = 0.071;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.OzdoevaLM.Sprint3.Task1.V23/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.OzdoevaLM.Sprint3.Task1.V23.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint3.Task1.V23
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполниа: Оздоева Л.М. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполниа: Оздоева Л.М. | ИИПб-25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет произведение *");
+            Console.WriteLine("* ряда по формуле, при х=5                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 5;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("старт шага = " + startValue);
+            Console.WriteLine("конец шага = " + stopValue);
+
+
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
